308e9c3bc0a39c73568a59afcb8ba908b0ee38fd
WhosArjun <arjun.arunkumar.v@gmail.com>
2026-07-08 10:31:27 -0400

    Linear Regression from scratch

---
+++ Linear Regression/LnReg.py
@@ -0,0 +1,46 @@
+import pandas as pd
+import matplotlib.pylot as plt
+
+data = pd.read_csv('data.csv')
+
+#How much we're off from the actual result
+def loss_function(m,b,points):
+    total_error = 0
+    for i in range(len(points)):
+        x = points.iloc[i].studytime
+        y = points.iloc[i].score
+        total_error += (y - (m*x + b)) ** 2
+    total_error/float(len(points))
+
+def gradient_descent(m_now, b_now, points, learning_rate):
+    m_gradient = 0
+    b_gradient = 0
+
+    n = len(points)
+
+    for i in range(n):
+        x = points.iloc[i].studytime
+        y = points.iloc[i].score
+
+        #Partial derivative of loss function with respect to m and b
+        m_gradient += (-2/n) * x * (y-(m_now*x + b_now))
+        b_gradient += (-2/n) * (y-(m_now*x + b_now)) #b is the same without x
+
+    m = m_now - m_gradient * learning_rate
+    b = b_now - b_gradient * learning_rate
+    return m,b 
+
+m = 0
+b = 0
+L = 0.0001
+epochs = 1000
+
+for i in range(epochs):
+    if i % 50 == 0 :
+        print(f"Epoch: {i}")
+    m,b = gradient_descent(m,b,data,L)
+
+print(m,b)
+plt.scatter(data.studytime, data.score, color = "black")
+plt.plot(list(range(20,80)), [m*x + b for x in range(20,80)], color = "red")
+plt.show()
